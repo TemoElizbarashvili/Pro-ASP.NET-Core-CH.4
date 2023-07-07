cab16f8e3ad79fbf4ca228687a25081d3869395d
TemoElizbarashvili <115801621+TemoElizbarashvili@users.noreply.github.com>
2023-07-07 21:45:03 +0400

    Init models and project

---
+++ Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Person
+    {
+        public long PersonId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+
+    }
+}
